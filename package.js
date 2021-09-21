282342dcaae497c4040a1f9cdb9aa88d2e89f5d2
Grant Hobbs <grant@gwhobbs.com>
2021-09-21 13:21:27 -0500

    Bump dependency versions to support Meteor 2.3

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 Package.describe({
   name: 'ermlab:office365-oauth',
-  version: '0.2.0',
+  version: '0.3.1',
   summary: 'Microsoft Office 365 OAuth flow',
   git: 'https://github.com/Ermlab/meteor-office365-oauth.git',
   documentation: 'README.md',
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.5.1');
+  api.versionsFrom(['1.5.1', '2.3']);
 
   api.use('ecmascript');
   api.use('oauth2', ['client', 'server']);
